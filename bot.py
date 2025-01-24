28c437e8e1e4b844adffed60031c6720e6addfa6
soniksoft <soniksoft1@gmail.com>
2025-01-24 20:07:01 +1000

    Обновил код для python-telegram-bot 20.0

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 # Команда /start
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Привет! Я бот для игры Ash & Abyss.')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Привет! Я бот для игры Ash & Abyss.')
 
 # Основная функция
 def main():
     # Вставь сюда свой токен
     token = 'ТВОЙ_ТОКЕН'
-    updater = Updater(token)
 
-    # Регистрация команд
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
+    # Создаём приложение и передаём токен
+    application = Application.builder().token(token).build()
 
-    # Запуск бота
-    updater.start_polling()
-    updater.idle()
+    # Регистрируем команду /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
